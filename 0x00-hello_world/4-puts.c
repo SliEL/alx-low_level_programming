1ad5ed71eae28335ded45391b6f35b4142274f2d
SliEl <elmansourisliman@gmail.com>
2023-03-16 13:49:30 -0700

    task 4 puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+	/* printing a message with the puts function */
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
-	return 0;
+	return (0);
 }
 
