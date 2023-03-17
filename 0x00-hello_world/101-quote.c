daa63ab080b44eb6351cff9d728802776be4c423
SliEl <elmansourisliman@gmail.com>
2023-03-17 10:25:50 -0700

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - print to standard error
+*Return: always retur 1 (error)
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
