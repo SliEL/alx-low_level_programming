57a1b008fd276822edce525587480b3e7988d846
SliEl <elmansourisliman@gmail.com>
2023-03-18 13:04:29 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always return 0
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 102;
+	int q = 114;
+
+	while (n < 101)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m < 113)
+	{
+		putchar(m);
+		m++;
+	}
+	while (q < 123)
+	{
+		putchar(q);
+		q++;
+	}
+	putchar('\n');
+	return (0);
+}
