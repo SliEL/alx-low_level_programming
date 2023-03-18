c5bd634ac2fdb4733dcf1e0a9a824ff424179217
SliEl <elmansourisliman@gmail.com>
2023-03-18 13:08:53 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,23 +6,16 @@
 int main(void)
 {
 	int n = 97;
-	int m = 102;
-	int q = 114;
 
-	while (n < 101)
+	while (n < 123)
 	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
 		putchar(n);
 		n++;
-	}
-	while (m < 113)
-	{
-		putchar(m);
-		m++;
-	}
-	while (q < 123)
-	{
-		putchar(q);
-		q++;
 	}
 	putchar('\n');
 	return (0);
