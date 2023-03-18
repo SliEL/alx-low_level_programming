bd6ed9cdc05b255867b06d56a833de76930f2d09
SliEl <elmansourisliman@gmail.com>
2023-03-18 12:43:54 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always return 0
+ */
+int main(void)
+{
+        int n = 97;
+        int m = 65;
+
+        while (n < 123)
+        {
+                putchar(n);
+                n++;
+        }
+        while (m < 91)
+        {
+                putchar(n);
+                n++;
+        }
+        putchar('\n');
+        return (0);
+}
