a370bd1b40baf4838d241f977e0732691a20f2c3
SliEl <elmansourisliman@gmail.com>
2023-03-18 13:33:02 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n > 96)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
