112b34b02f4da5ff902bdb6d581dd7fbb1d8bd72
SliEl <elmansourisliman@gmail.com>
2023-03-18 13:38:28 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 48;
+	int m = 97;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m < 103)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
