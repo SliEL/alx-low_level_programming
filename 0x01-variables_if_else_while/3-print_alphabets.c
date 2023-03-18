1944d90003cf6449034be513c68e7a77daba1941
SliEl <elmansourisliman@gmail.com>
2023-03-18 12:52:34 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,19 +5,19 @@
  */
 int main(void)
 {
-        int n = 97;
-        int m = 65;
+	int n = 97;
+	int m = 65;
 
-        while (n < 123)
-        {
-                putchar(n);
-                n++;
-        }
-        while (m < 91)
-        {
-                putchar(m);
-                m++;
-        }
-        putchar('\n');
-        return (0);
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m < 91)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
 }
