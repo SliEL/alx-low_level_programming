d97b9137d9f418c56a8f2f58c68951ea4f222632
SliEl <elmansourisliman@gmail.com>
2023-03-21 15:43:59 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * add - sum of two numbers
+ * @n: integer
+ * @m: integer
+ * Return: sum
+ */
+int add(int n, int m)
+{
+	int sum = n + m;
+	int digits[10];
+	int i = 0;
+
+	if (sum == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+	while (sum != 0)
+	{
+		digits[0] = sum % 10;
+		sum /= 10;
+		i++;
+	}
+	for (int j = i - 1; j >= 0; j--)
+	{
+		_putchar(digits[j] + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
+
