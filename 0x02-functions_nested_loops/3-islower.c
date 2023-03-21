1d01bca9a8a3237fa3a0bfff0407d65e7ba78abd
SliEl <elmansourisliman@gmail.com>
2023-03-21 12:34:17 -0700

    _islower task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * int _islowe - function
+ * _islower - function
  * Return: 1 if lowercase and 0 otherwise
  */
 int _islower(int c)
 {
-	return (c <= 'a' && c >= 'z');
+	return (c >= 'a' && c <= 'z');
 }
