2fad918e1c273c582f07a3152ba99cad095566bf
SliEl <elmansourisliman@gmail.com>
2023-03-21 15:56:30 -0700

    task10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,27 +7,6 @@
  */
 int add(int n, int m)
 {
-	int sum = n + m;
-	int digits[10];
-	int i = 0, j;
-
-	if (sum == 0)
-	{
-		_putchar('0');
-		_putchar('\n');
-		return (0);
-	}
-	while (sum != 0)
-	{
-		digits[0] = sum % 10;
-		sum /= 10;
-		i++;
-	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(digits[j] + '0');
-	}
-	_putchar('\n');
-	return (0);
+	return (n + m);
 }
 
