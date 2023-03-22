d846e0f86067bc316a2972adad83cd5d6aa94431
SliEl <elmansourisliman@gmail.com>
2023-03-22 09:03:08 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{       
 			printf("%d", i);
-			if (n != 98)
+			if (i != 98)
 			{       
 				printf(", ");
 			}       
@@ -31,7 +31,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{       
 			printf("%d", i);
-			if (n != 98)
+			if (i != 98)
 			{       
 				printf(", ");
 			}       
