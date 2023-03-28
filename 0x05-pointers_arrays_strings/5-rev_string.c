23fde6e4b51280f5b3011fe0bf172ec34bf8d072
SliEl <elmansourisliman@gmail.com>
2023-03-28 08:55:55 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string  - reverse a string
+ * @s: pointer
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	char *e = s;
+	char temp;
+
+	/* set e to point on the last char of the string */
+	while (*e != '\0')
+		e++;
+	e--;
+
+	/* swap chars using the temp variable */
+	while (e != s)
+	{
+		temp = *e;
+		*e = *s;
+		*s = temp;
+		s++;
+		e--;
+	}
+}
