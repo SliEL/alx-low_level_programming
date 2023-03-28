e65a25f37da2c0e169923e65ea91480ec7238174
SliEl <elmansourisliman@gmail.com>
2023-03-28 10:33:30 -0700

    puts2 function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,12 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	char *p = str;
+
+	while (*p != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		_putchar(*p);
+		p += 2;
 	}
 	_putchar('\n');
 }
