d8a487eb084c24103b6c39699f5973b787e842f2
SliEl <elmansourisliman@gmail.com>
2023-03-28 18:33:26 -0700

    advanced tasks 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to number
+ * @s: pointer
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign;
+	int digit;
+
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign = -1;
+			s++;
+		}
+		else if (*s == '+')
+		{
+			sign = 1;
+			s++;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit = *s - '0';
+			result = result * 10 + digit;
+			s++
+		}
+		else
+		{
+			s++;
+			continue;
+		}
+	}
+	return (sign * result);
+}
