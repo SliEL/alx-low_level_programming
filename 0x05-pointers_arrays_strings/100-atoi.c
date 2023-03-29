9d6c882feae411434013d1c0f621f2210a913f0e
SliEl <elmansourisliman@gmail.com>
2023-03-28 19:02:15 -0700

    hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,8 +7,9 @@
 int _atoi(char *s)
 {
 	int result = 0;
-	int sign;
+	int sign = 1;
 	int digit;
+	int max_div_10 = INT_MAX / 10;
 
 	while (*s)
 	{
@@ -25,6 +26,10 @@ int _atoi(char *s)
 		else if (*s >= '0' && *s <= '9')
 		{
 			digit = *s - '0';
+			if (result > max_div_10 || (result == max_div_10 && digit >= 8))
+			{
+				return sign = 1 ? INT_MAX : INT_MIN;
+			}
 			result = result * 10 + digit;
 			s++;
 		}
