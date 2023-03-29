2571ff63bfc13d4a32b260611a0fdbe9713435fb
SliEl <elmansourisliman@gmail.com>
2023-03-29 10:55:04 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - copy n chars to dest
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *p = dest;
+
+	while (i < n)
+	{
+		*p++ = *src++;
+		i++;
+	}
+	return (dest);
+}
