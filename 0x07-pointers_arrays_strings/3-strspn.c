04bea5d7d6fe04e7c9d1d87a941f31a1573acfe2
SliEl <elmansourisliman@gmail.com>
2023-04-03 11:06:16 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strspn - Returns the number of bytes in the initial segment of s which consist only of bytes from accept
+ * @s:pointer
+ * @accept: pointer
+ * Return: integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	unsigned int len = 0;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] >= '\0'\; j++)
+		{
+			if (s[i] == accept[j])
+				len++;
+		}
+	}
+	return (len);
+}
