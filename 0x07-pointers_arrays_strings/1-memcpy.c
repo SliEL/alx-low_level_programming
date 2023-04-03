5fe0ecb38122187f0d86b98d1448d04f15c1b63c
SliEl <elmansourisliman@gmail.com>
2023-04-03 10:08:41 -0700

    copy memory erea - task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * char _memcpy - copy memory area
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p = dest;
+
+	while (n-- > 0)
+	{
+		*p++ = *src;
+	}
+	return (dest);
+}
