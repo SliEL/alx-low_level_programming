6720a532513b68cbba666883b62901f9fe92ebe7
SliEl <elmansourisliman@gmail.com>
2023-04-03 11:33:47 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - search astring 
+ * @s: pointer
+ * @accept: pointer
+ * Return: pointer
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		while(*accept)
+		{
+			if (*s == *accept)
+				return (s);
+			accept++;
+		}
+		s++;
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ int _putchar(char c);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 
 #endif /*MAIN_H*/
