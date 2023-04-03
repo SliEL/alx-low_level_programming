44e91b14756fa62058635e8d8b9312e946250500
SliEl <elmansourisliman@gmail.com>
2023-04-03 11:16:11 -0700

    task5

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,7 +7,7 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	int i, j;
 	unsigned int len = 0;
 
 	for (i = 0; s[i] >= '\0'; i++)
@@ -15,7 +15,10 @@ unsigned int _strspn(char *s, char *accept)
 		for (j = 0; accept[j] >= '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				len++;
+				break;
+			}
 		}
 	}
 	return (len);
