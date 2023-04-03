7e942e724aa9fc7f5f0436057992f88c01279fa9
SliEl <elmansourisliman@gmail.com>
2023-04-03 10:35:09 -0700

    find first occurance of a char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - locates a char in string (first occurance)
+ * @s: pointer
+ * @c: char
+ * Return: Null or pointer to first occurance
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
