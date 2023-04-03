bda6be61148c96c1d1077ebd2bf9dcf1f660925f
SliEl <elmansourisliman@gmail.com>
2023-04-03 15:24:05 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,8 +17,10 @@ char *_strstr(char *haystack, char *needle)
 			for (i = 0; needle[i]; i++)
 			{
 				if (haystack[i] != needle[i])
+				{
 					t = 0;
 					break;
+				}
 			}
 			if (t)
 				return (haystack);
