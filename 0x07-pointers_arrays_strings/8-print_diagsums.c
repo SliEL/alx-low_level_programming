1ef8374bff32a44997304814dc58cd313daf74a9
SliEl <elmansourisliman@gmail.com>
2023-04-03 17:13:33 -0700

    diag sums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,12 +10,12 @@ void print_diagsums(int *a, int size)
 {
 	int sum1 = 0;
 	int sum2 = 0;
-	int i, j;
+	int i;
 
 	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + i*size + i);
 		sum2 += *(a + i*size + size - i - 1);
 	}
-	printf("%d, %d", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
