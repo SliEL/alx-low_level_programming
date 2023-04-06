3f7dedd4ea63d66bf7eb13630145d91552dc784d
SliEl <elmansourisliman@gmail.com>
2023-04-06 05:37:35 -0700

    find sqrt recuresively

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,7 @@
 #include "main.h"
+
+int _sqrt(int n, int i);
+
 /**
  * _sqrt_recursion - calculates sqrt of an intger
  * @n: integer
@@ -6,4 +9,22 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, 0));
+}
+
+/**
+ * _sqrt - finds the natural sqrt of n recursively
+ * @n: integer
+ * @i: integer ietartor
+ * Return: the sqrt
+ */
+int _sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
