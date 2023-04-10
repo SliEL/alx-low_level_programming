447d0181fc7a9a838d71f0a17d26384131a7e5eb
SliEl <elmansourisliman@gmail.com>
2023-04-10 11:44:42 -0700

    print mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * _atoi - converts string to int
+ * @str: pointer
+ * Return: int
+ */
+
+int _atoi(const char *str)
+{
+	int sign = 1;
+	int result = 0;
+
+	while (*str == ' ')
+	{
+		str++;
+	}
+
+
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+		{
+			sign = -1;
+		}
+
+		str++;
+	}
+
+
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+
+	return (sign * result);
+}
+
+
 /**
  * main - multiplies two numbers
  * @argc: number of args
@@ -10,7 +49,7 @@ int main(int argc, char *argv[])
 {
 	if (argv[1] && argv[2])
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
 		return (0);
 	}
 	else
