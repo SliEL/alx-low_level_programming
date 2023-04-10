00ddace300ad089479ee3c0d8193a543591ea2cf
SliEl <elmansourisliman@gmail.com>
2023-04-10 10:50:04 -0700

    print the num of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the number of args passed in CL
+ * @argc: number of args
+ * @argv: vector of strings (arguments)
+ * Return: 0;
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
