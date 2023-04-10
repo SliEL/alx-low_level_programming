579139882fbf94a9027edd35adc3cd036b790a6b
SliEl <elmansourisliman@gmail.com>
2023-04-10 10:52:06 -0700

    print the num of args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: vector of strings (arguments)
  * Return: 0;
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
