a56cee054b89825593a8382e731152a777ec340b
SliEl <elmansourisliman@gmail.com>
2023-04-13 17:46:35 -0700

    allocate mem for array using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ptr = malloc(nmemb * size);
 
-	if (!ptr || nmemb == 0 || size == 0)
+	if (ptr == NULL || nmemb == 0 || size == 0)
 		return (NULL);
 
 	for (i = 0; i < nmemb * size; i++)
