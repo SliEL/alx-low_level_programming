a8fd7499bea4947ae93fcf8951ee149333cd70d2
Sliman <Elmansourisliman@gmail.com>
2023-04-27 22:56:07 +0100

    add new node to the begging of the ll

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - add a node to the end of the ll.
+ * @head: pointer to the pointer to the head
+ * @str: node value
+ * Return: address of new element or NUll.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+
+	/* calculate len of str */
+	while (!new)
+		return (NULL);
+	/* insert new node in the beggining of the list*/
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
