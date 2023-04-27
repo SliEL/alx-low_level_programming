2e02b17ec9270879baa007fefb3fc1cc70365897
Sliman <Elmansourisliman@gmail.com>
2023-04-27 22:17:35 +0100

    prints the len of list of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * size_t list_len - function that prints the len of the list
+ * @h: pointer to the list
+ * Return: len of the list of nodes.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
