436a060e4398183f964882f7210e75a4532424e8
Sliman <Elmansourisliman@gmail.com>
2023-04-28 20:29:30 +0100

    task 0 more linked lists project

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		n++;
+		num++;
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (n);
+	return (num);
 }
