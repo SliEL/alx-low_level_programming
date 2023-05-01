09a78b540411c44e01639275dfd3677c200bc5a5
Sliman <Elmansourisliman@gmail.com>
2023-05-01 22:28:24 +0100

    frees the ll , function accepts **head as param

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_listint2 - free the ll
+ * @head: pointer to the pointer of the head
+ * Return: nothing - void
+ */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *current, *next;
