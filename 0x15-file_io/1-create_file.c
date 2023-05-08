6f9e104fdb72ea1f2335a66e13205a7661bd78c5
Sliman <Elmansourisliman@gmail.com>
2023-05-08 21:19:05 +0100

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file
+ * @filename: ptr to the file
+ * @text_content: the text write to the file
+ *
+ * Return: 1 success || -1 failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, w, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+			len++;
+
+		w = write(fd, text_content, len);
+
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
