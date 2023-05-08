eb2cc860da19c58fcd332b0b8c80013e9dc5370a
Sliman <Elmansourisliman@gmail.com>
2023-05-08 19:56:49 +0100

    read txt file and write to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads textfile & prints it to the POSIX standard output
+ * @filename: ptr to the file
+ * @letters: num of letters to read
+ *
+ * Return: num of bytes read or 0 if ptr is NULL or fail
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t fd, w, t;
+	char *buffer = malloc(letters);
+
+	if (!filename || !buffer)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	t = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, t);
+
+	free(buffer);
+	close(fd);
+	return (w);
+}
