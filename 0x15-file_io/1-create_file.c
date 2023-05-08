7c4b34f823c1641d6a896726194af772fca0f717
Sliman <Elmansourisliman@gmail.com>
2023-05-08 21:20:16 +0100

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,12 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i, w, len = 0;
+	int fd, i, w, len = 0;
 
 	if (!filename)
 		return (-1);
 
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
