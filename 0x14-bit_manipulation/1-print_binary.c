1971a6243487863ef247d118477e21dc93e93760
Sliman <Elmansourisliman@gmail.com>
2023-07-06 19:56:16 +0100

    bit manipulation projext

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary rep of a num
+ * @n: number
+ * Return: nothing (void)
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+
+	for (i = 63; i >= 0; i--)
+	{
+		unsigned long int mask = n >> i;
+
+		if (1 & mask)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count > 0)
+		{
+			_putchar('0');
+		}
+	}
+
+	if (count == 0)
+	{
+		_putchar('0');
+	}
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+read me file for the bit manipulation project 0x14
