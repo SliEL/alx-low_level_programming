5b6793f4376068bc4ee1bd77bb4a8a0126076016
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-08-17 19:31:16 +0100

    0x17 task 0 doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* print_dlistint - prints all node of dll
+* @h: head of the dll
+* Return: the number of nodes in the dll
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+    int count = 0;
+
+    if (h == NULL)
+    {
+        return (count);
+    }
+
+    while (h->prev != NULL)
+    {
+        h = h->prev;
+    }
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        count++;
+        h = h->next;
+    }
+
+    return (count);
+}
